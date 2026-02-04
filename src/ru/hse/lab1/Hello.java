a07d1b2022a12aa881b407c0868685d008ec58cc
morr66 <yar.bukin@mail.ru>
2026-02-04 10:54:09 +0300

    level2

--- src/ru/hse/lab1/Hello.java
+++ src/ru/hse/lab1/Hello.java
@@ -1,7 +1,13 @@
 package ru.hse.lab1;
 
+import ru.hse.lab1.second.Person;
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        Person person = new Person("Oleg", 18);
+        System.out.println(person.name);
+        System.out.println(person.age);
+
     }
 }
